9f2e772c9630346ed5d3855386b3f067fecac47f
kevina choolhun <kevina.choolhun@gmail.com>
2011-06-14 14:12:10 +0400

    adding files
    Signed-off-by: kevina choolhun <kevina.choolhun@gmail.com>

---
+++ src/com/kevinachoolhun/myvicinity/MyVicinityServlet.java
@@ -0,0 +1,13 @@
+package com.kevinachoolhun.myvicinity;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MyVicinityServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world fromKevina");
+	}
+}
